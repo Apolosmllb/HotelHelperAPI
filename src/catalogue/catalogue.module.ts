44b0f186f6f112d8b605c84beb42e3e7530001de
ClinderSGT <93946818+ClinderSGT@users.noreply.github.com>
2021-11-20 23:40:08 -0500

    add a new module and entity

--- src/catalogue/catalogue.module.ts
+++ src/catalogue/catalogue.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Searching } from './searching.entity';
+import { CatalogueService } from './catalogue.service';
+import { CatalogueController } from './catalogue.controller';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Searching])],
+  providers: [CatalogueService],
+  controllers: [CatalogueController],
+})
 export class CatalogueModule {}

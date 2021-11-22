6311623e52d38378e9a8b6287f5ea14cbe9b2cbd
JoaquinSM13 <u201910511@upc.edu.pe>
2021-11-22 14:34:38 -0500

    add serviceManagement

--- src/service-management/customerservice.entity.ts
+++ src/service-management/customerservice.entity.ts
@@ -1,14 +1,14 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 @Entity('customers')
-export class Customer {
+export class CustomerService {
   @PrimaryGeneratedColumn()
   id:number;
   @Column('first_name')
   firstName : string;
   @Column('last_name')
   lastName: string;
-  @Column('room_number')
-  roomNumber:number;
+  @Column('room_number_service')
+  roomNumberService:number;
   @Column('phone_number')
   phoneNumber:number;
 }

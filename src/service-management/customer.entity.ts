4261647c11ea3f94fc8e90f16d8223c23300bdc4
JoaquinSM13 <u201910511@upc.edu.pe>
2021-11-22 13:44:21 -0500

    add serviceManagement

---
+++ src/service-management/customer.entity.ts
@@ -0,0 +1,14 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity('customers')
+export class Customer {
+  @PrimaryGeneratedColumn()
+  id:number;
+  @Column('first_name')
+  firstName : string;
+  @Column('last_name')
+  lastName: string;
+  @Column('room_number')
+  roomNumber:number;
+  @Column('phone_number')
+  phoneNumber:number;
+}
--- src/service-management/service-management.module.ts
+++ src/service-management/service-management.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { Inventory } from './inventory.entity';
 import { ServiceManagementService } from './service-management.service';
 import { ServiceManagementController } from './service-management.controller';
+import { Customer } from './customer.entity';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Inventory])],
+  imports:[TypeOrmModule.forFeature([Inventory,Customer])],
   providers: [ServiceManagementService],
   controllers: [ServiceManagementController],
 })

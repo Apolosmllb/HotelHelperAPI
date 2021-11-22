a6058f551d9f83ccf15444f95f32cc6e60bea8c8
KalidPalacios <86016214+KalidPalacios@users.noreply.github.com>
2021-11-22 13:29:22 -0500

    add app.module.ts changes

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -7,25 +7,25 @@ import {
   Post,
   Res,
 } from '@nestjs/common';
-import {PaymentService} from "./payment.service";
-import {Pay} from "./pay.entity";
+import { PaymentService } from './payment.service';
+import { Pay } from './pay.entity';
 
 @Controller('pays')
 export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
   @Post()
-  async createPay(@Res() response, @Body() pay:Pay){
+  async createPay(@Res() response, @Body() pay: Pay) {
     const newPay = await this.paymentService.createPay(pay);
-    return response.status(HttpStatus.CREATED).json({newPay});
+    return response.status(HttpStatus.CREATED).json({ newPay });
   }
   @Get()
-  async fetchAll(@Res() response){
+  async fetchAll(@Res() response) {
     const pays = await this.paymentService.findAll();
-    return response.status(HttpStatus.OK).json({pays});
+    return response.status(HttpStatus.OK).json({ pays });
   }
   @Get('/:id')
-  async findById(@Res() response, @Param('id') id){
+  async findById(@Res() response, @Param('id') id) {
     const pay = await this.paymentService.createPay(id);
-    return response.status(HttpStatus.CREATED).json({pay});
+    return response.status(HttpStatus.CREATED).json({ pay });
   }
 }
